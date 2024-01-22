f3790fd016005361b484ee772e1ad740d34fd1bb
TIMSCOMP\Tim <timratsko909@gmail.com>
2024-01-21 23:24:06 -0800

    Added a dino animation

--- main.js
+++ main.js
@@ -2,9 +2,14 @@ const gameEngine = new GameEngine();
 
 const ASSET_MANAGER = new AssetManager();
 
+ASSET_MANAGER.queueDownload("./Walk.png");
+
 ASSET_MANAGER.downloadAll(() => {
 	const canvas = document.getElementById("gameWorld");
 	const ctx = canvas.getContext("2d");
+	ctx.imageSmoothingEnabled = false;
+
+	gameEngine.addEntity(new Walk(gameEngine));
 
 	gameEngine.init(ctx);
 
